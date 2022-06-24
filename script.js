37eece9c6c38ef97d23b4b67fa56c7226984705f
LightningDarkMatter <70206474+LightningDarkMatter@users.noreply.github.com>
2022-06-24 20:41:35 +0000

    Added Comments

--- script.js
+++ script.js
@@ -1,49 +1,54 @@
+// Get time the page was loaded.
 var start = new Date().getTime();
-            var end = new Date().getTime();
-            var time = end - start;
+// Get time when shape was clicked.
+var end = new Date().getTime();
+// Work out how long it took to click on the shape.
+var time = end - start;
 
-            function randomColor() {
-                var letters = '0123456789ABCDEF';
-                var color = '#';
-                for (var i = 0; i < 6; i++) {
-                    color += letters[Math.floor(Math.random() * 16)];
-                }
-                return color;
-            }
+// Generate a random colour. This is used to generate the background colour of the shape.
+function randomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
-            function makeShapeAppear() {
-                var top = Math.random() * 300;
-                var left = Math.random() * 300;
-                var size = Math.random() * 300;
+// This function is used to generate the shape. By using the randomColor function, the background colour of the shape is generated. The size and shape of the shape displayed are randomised.
+function makeShapeAppear() {
+  var top = Math.random() * 300;
+  var left = Math.random() * 300;
+  var size = Math.random() * 300;
 
-                if (Math.random() > 0.5) {
-                    document.getElementById('shape').style.borderRadius = '50%';
-                } else {
-                    document.getElementById('shape').style.borderRadius = '0';
-                }
+  if (Math.random() > 0.5) {
+    document.getElementById("shape").style.borderRadius = "50%";
+  } else {
+    document.getElementById("shape").style.borderRadius = "0";
+  }
 
-                document.getElementById("shape").style.top = top + "px";
-                document.getElementById("shape").style.left = left + "px";
-                document.getElementById("shape").style.width = size + "px";
-                document.getElementById("shape").style.height = size + "px";
-                document.getElementById("shape").style.backgroundColor = randomColor();
-                document.getElementById("shape").style.display = "block";
-                start = new Date().getTime();
-                
-            }
+  document.getElementById("shape").style.top = top + "px";
+  document.getElementById("shape").style.left = left + "px";
+  document.getElementById("shape").style.width = size + "px";
+  document.getElementById("shape").style.height = size + "px";
+  document.getElementById("shape").style.backgroundColor = randomColor();
+  document.getElementById("shape").style.display = "block";
+  start = new Date().getTime();
+}
 
-            function shapeAppear() {
-                setTimeout(makeShapeAppear, Math.random() * 2000);
-            }
+// This function randomises the amount of time that passes between each shape appearing.
+function shapeAppear() {
+  setTimeout(makeShapeAppear, Math.random() * 2000);
+}
 
-            shapeAppear();
+shapeAppear();
 
-            document.getElementById("shape").onclick = function() {
-                document.getElementById("shape").style.display = "none";
-                end = new Date().getTime();
-                time = (end - start) / 1000;
-                document.getElementById("time").innerHTML = time + "s";
+// Once a shape is clicked, this updates the html code to display the time it took to click on the shape.
+document.getElementById("shape").onclick = function () {
+  document.getElementById("shape").style.display = "none";
+  end = new Date().getTime();
+  time = (end - start) / 1000;
+  document.getElementById("time").innerHTML = time + "s";
 
-                shapeAppear();
-            };
-            
+  shapeAppear();
+};
